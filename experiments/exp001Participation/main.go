9869dca893d1ff5b8b5c19007159e3b073330263
Grant Nelson <grantnelson1@gmail.com>
2021-02-03 01:39:03 -0700

    Made it super simple to just get it working for type discovery. Then will add in metric measurements

--- experiments/exp001Participation/main.go
+++ experiments/exp001Participation/main.go
@@ -2,7 +2,17 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"io/ioutil"
+	"log"
 	"os"
+	"path"
+	"sort"
+	"strings"
 )
 
 func main() {
@@ -13,7 +23,39 @@ func main() {
 	}
 	basePath := os.Args[1]
 
-	//startTime := time.Now()
-	fmt.Println("Path:", basePath)
-	//fmt.Printf("Done (%d files in %s)\n", len(p.Filenames()), time.Since(startTime).String())
+	// Find all the
+	fileInfo, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		panic(err)
+	}
+	filenames := []string{}
+	for _, info := range fileInfo {
+		filename := path.Clean(path.Join(basePath, info.Name()))
+
+		if strings.HasSuffix(filename, ".go") && !strings.HasSuffix(filename, "_test.go") {
+			filenames = append(filenames, filename)
+		}
+	}
+	sort.Strings(filenames)
+
+	fileset := token.NewFileSet()
+	files := []*ast.File{}
+	for _, filename := range filenames {
+		f, err := parser.ParseFile(fileset, filename, nil, parser.ParseComments)
+		if err != nil {
+			panic(err)
+		}
+		files = append(files, f)
+	}
+
+	conf := types.Config{Importer: importer.Default()}
+	pkg, err := conf.Check(basePath, fileset, files, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Package  %q\n", pkg.Path())
+	fmt.Printf("Name:    %s\n", pkg.Name())
+	fmt.Printf("Imports: %s\n", pkg.Imports())
+	fmt.Printf("Scope:   %s\n", pkg.Scope())
 }
