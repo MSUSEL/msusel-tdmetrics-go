870507cdaaa0146537ca64b6a1a024616d13a29e
Grant Nelson <grantnelson1@gmail.com>
2024-07-08 21:53:57 -0600

    Update context.go

--- goAbstractor/internal/jsonify/context.go
+++ goAbstractor/internal/jsonify/context.go
@@ -2,90 +2,98 @@ package jsonify
 
 import "maps"
 
+type contextKey int
+
+const (
+	keyMinimized contextKey = iota
+	keyShort
+	keyKindShown
+	keyReceiverShown
+	keyReferenceShown
+)
+
 type Context struct {
-	state map[string]bool
+	state map[contextKey]bool
 }
 
 func NewContext() *Context {
-	return &Context{
-		state: map[string]bool{},
-	}
+	return &Context{state: nil}
 }
 
-func (c *Context) copyAndSet(name string, state bool) *Context {
+func (c *Context) copyAndSet(key contextKey, state bool) *Context {
 	c2 := &Context{
 		state: maps.Clone(c.state),
 	}
 	if c2.state == nil {
-		c2.state = map[string]bool{}
+		c2.state = map[contextKey]bool{}
 	}
-	c2.state[name] = state
+	c2.state[key] = state
 	return c2
 }
 
 // SetMinimize sets if the output JSON should be minimized.
 func (c *Context) SetMinimize(min bool) *Context {
-	return c.copyAndSet(`minimized`, min)
+	return c.copyAndSet(keyMinimized, min)
 }
 
 // IsMinimized indicates that the output JSON should be minimized.
 func (c *Context) IsMinimized() bool {
-	return c.state[`minimized`]
+	return c.state[keyMinimized]
 }
 
 // Short indicates that objects should output only an index or name
 // as a reference to the rest of the object defined elsewhere.
 func (c *Context) Short() *Context {
-	return c.copyAndSet(`short`, true)
+	return c.copyAndSet(keyShort, true)
 }
 
 // Long indicates that objects should output the whole object,
 // not the shortened version.
 func (c *Context) Long() *Context {
-	return c.copyAndSet(`short`, false)
+	return c.copyAndSet(keyShort, false)
 }
 
 // IsShort indicates that objects should output only an index or name
 // as a reference to the rest of the object defined elsewhere.
 func (c *Context) IsShort() bool {
-	return c.state[`short`]
+	return c.state[keyShort]
 }
 
 // ShowKind indicates that the kind field should be added to the output model.
 func (c *Context) ShowKind() *Context {
-	return c.copyAndSet(`kindShown`, true)
+	return c.copyAndSet(keyKindShown, true)
 }
 
 // HideKind indicates that the "kind" field can be skipped.
 func (c *Context) HideKind() *Context {
-	return c.copyAndSet(`kindShown`, false)
+	return c.copyAndSet(keyKindShown, false)
 }
 
 // IsKindShown indicates that the kind field should be added to the output model.
 func (c *Context) IsKindShown() bool {
-	return c.state[`kindShown`]
+	return c.state[keyKindShown]
 }
 
 // ShowReceiver sets if the methods should include receiver information
 // in the object model. This is for debugging purposes.
 func (c *Context) ShowReceiver(show bool) *Context {
-	return c.copyAndSet(`receiverShown`, show)
+	return c.copyAndSet(keyReceiverShown, show)
 }
 
 // IsReceiverShown indicates that methods should include receiver information
 // in the object model. This is for debugging purposes.
 func (c *Context) IsReceiverShown() bool {
-	return c.state[`receiverShown`]
+	return c.state[keyReceiverShown]
 }
 
 // ShowReference sets if the typeDef reference should include the
 // reference name in the object model. This is for debugging purposes.
 func (c *Context) ShowReference(show bool) *Context {
-	return c.copyAndSet(`referenceShown`, show)
+	return c.copyAndSet(keyReferenceShown, show)
 }
 
 // IsReferenceShown indicates that typeDef reference should include the
 // reference name in the object model. This is for debugging purposes.
 func (c *Context) IsReferenceShown() bool {
-	return c.state[`referenceShown`]
+	return c.state[keyReferenceShown]
 }
