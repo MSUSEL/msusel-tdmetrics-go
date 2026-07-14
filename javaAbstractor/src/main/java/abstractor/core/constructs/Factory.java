2d012085579b3947cace36eab3f876c38cab498d
Grant Nelson <grantnelson1@gmail.com>
2026-07-14 00:25:15 -0600

    adding log

--- javaAbstractor/src/main/java/abstractor/core/constructs/Factory.java
+++ javaAbstractor/src/main/java/abstractor/core/constructs/Factory.java
@@ -11,7 +11,7 @@ import abstractor.core.require.Require;
 import abstractor.core.tools.ElementKey;
 
 public class Factory<T extends Construct> implements Jsonable {
-    static private final boolean logCreate = true;
+    static private final boolean logCreate = false;
 
     private final ConstructKind conKind;
 
