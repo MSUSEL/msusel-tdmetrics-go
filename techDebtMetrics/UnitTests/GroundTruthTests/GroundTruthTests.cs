341cb16c5c11191acbb7381d9f3a6c4af2a4010d
Grant Nelson <grantnelson1@gmail.com>
2026-07-20 01:19:44 -0600

    Working on grand truth

--- techDebtMetrics/UnitTests/GroundTruthTests/GroundTruthTests.cs
+++ techDebtMetrics/UnitTests/GroundTruthTests/GroundTruthTests.cs
@@ -2,6 +2,7 @@
 using Commons.Data.Repo;
 using Constructs;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace UnitTests.GroundTruthTests;
 
@@ -14,6 +15,25 @@ public class GroundTruthTests {
         Assert.AreEqual(gt.ProjectId,  "org.apache:bcel");
         Assert.AreEqual(gt.GitLink,    "apache/commons-bcel");
         Assert.AreEqual(gt.CommitSha,  "6ed18c5bef0f5b93b54783a8e8fb2b9042da26ac");
+    }
+
+    [Test]
+    public void GroundTruthClasses() {
+        JavaTarget target = JavaTarget.CommonsBcel;
+        GT.GroundTruth gt = GT.GroundTruth.FromZip(Repo.MetricsZip, target);
+        Project proj = Project.FromFile(Repo.AbstractedJava(target));
+        SortedSet<string> gtNames     = [.. gt.Classes.Select(c => c.FullName)];
+        SortedSet<string> projNames   = [.. proj.ObjectDecls.Select(c => c.FullName)];
+        SortedSet<string> found       = [.. gtNames.Intersect(projNames)];
+        SortedSet<string> gtMissing   = [.. gtNames.Except(found)];
+        SortedSet<string> projMissing = [.. projNames.Except(found)];
+
+        Assert.Multiple(() => {
+            Assert.AreEqual(gtNames.Count,   gt.Classes.Count,       "Duplicate class names in ground truth data");
+            Assert.AreEqual(projNames.Count, proj.ObjectDecls.Count, "Duplicate class names in object declarations");
+            Assert.Zero(gtMissing.Count,   "Ground truth missing count:\n  " + string.Join("\n  ", gtMissing));
+            Assert.Zero(projMissing.Count, "Object declarations missing count:\n  " + string.Join("\n  ", projMissing));
+        });
     }
 
     [Test]
@@ -34,16 +54,14 @@ public class GroundTruthTests {
     }
 
     private void checkGroundTruth(GT.ClassMetrics gtObj, ObjectDecl obj) {
-        Assert.Multiple(() => {
-            foreach (GT.MethodMetrics gtMet in gtObj.Methods) {
-                MethodDecl? met = obj.Methods.FirstOrDefault(m => m.Location.LineNo == gtMet.Line);
-                if (met is null) {
-                    System.Console.WriteLine("Failed to find method " + gtMet.Name + " in " + gtObj.FullName);
-                    continue;
-                }
-                this.checkGroundTruth(gtObj, obj, gtMet, met);
+        foreach (GT.MethodMetrics gtMet in gtObj.Methods) {
+            MethodDecl? met = obj.Methods.FirstOrDefault(m => m.Location.LineNo == gtMet.Line);
+            if (met is null) {
+                System.Console.WriteLine("Failed to find method " + gtMet.Name + " in " + gtObj.FullName);
+                continue;
             }
-        });
+            this.checkGroundTruth(gtObj, obj, gtMet, met);
+        }
     }
 
     private void checkGroundTruth(GT.ClassMetrics gtObj, ObjectDecl obj, GT.MethodMetrics gtMet, MethodDecl met) {
