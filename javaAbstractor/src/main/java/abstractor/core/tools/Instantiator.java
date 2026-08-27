8ed3c981b2f92d162f15c573e1954f862c8ca42d
Grant Nelson <grantnelson1@gmail.com>
2026-08-27 11:07:26 -0600

    Update Instantiator.java

--- javaAbstractor/src/main/java/abstractor/core/tools/Instantiator.java
+++ javaAbstractor/src/main/java/abstractor/core/tools/Instantiator.java
@@ -7,38 +7,32 @@ import abstractor.core.json.*;
 import abstractor.core.require.Require;
 
 public class Instantiator {
-    private class Frame {
-        final public Frame prior;
+    public class Frame {
         final private TreeMap<Ref<? extends TypeDesc>, Ref<? extends TypeDesc>> subst = new TreeMap<>();
         final private ArrayList<Ref<? extends TypeDesc>> paramOrder = new ArrayList<>();
         private ArrayList<Ref<? extends TypeDesc>> argOrder = null;
         private int nestCount;
 
-        public Frame(Frame prior) {
-            this.prior = prior;
-        }
+        private Frame() {}
 
-        public void copyFromPrior() {
-            if (this.prior == null) return;
-            this.subst.putAll(this.prior.subst);
-            this.paramOrder.addAll(this.prior.paramOrder);
+        private void copyFrom(Frame other) {
+            this.subst.putAll(other.subst);
+            this.paramOrder.addAll(other.paramOrder);
             this.nestCount = this.paramOrder.size();
         }
 
-        public void copyImmediateFromPrior() {
-            if (this.prior == null) return;
-            final int fullCount = this.prior.paramOrder.size();
-            for (int i = this.prior.nestCount; i < fullCount; i++) {
-                final Ref<? extends TypeDesc> param = this.prior.paramOrder.get(i);
+        private void copyImmediateFrom(Frame other) {
+            final int fullCount = other.paramOrder.size();
+            for (int i = other.nestCount; i < fullCount; i++) {
+                final Ref<? extends TypeDesc> param = other.paramOrder.get(i);
                 this.paramOrder.add(param);
-                final Ref<? extends TypeDesc> sub = this.prior.subst.get(param);
+                final Ref<? extends TypeDesc> sub = other.subst.get(param);
                 this.subst.put(param, sub);
             }
             this.nestCount = this.paramOrder.size();
         }
 
-        public void add(Ref<? extends TypeDesc> param, Ref<? extends TypeDesc> arg) throws Exception {
-            if (this.prior != null) arg = this.prior.replace(arg);
+        private void add(Ref<? extends TypeDesc> param, Ref<? extends TypeDesc> arg) throws Exception {
             if (this.subst.put(param, arg) != null) {
                 final int index = this.paramOrder.indexOf(param);
                 if (index >= 0) {
@@ -96,30 +90,54 @@ public class Instantiator {
             return "[\n\t" + String.join("\n\t", parts) + "\n]";
         }
     }
-    
-    private Frame topFrame;
 
-    public Instantiator() {
-        this.topFrame = null;
+    private class StackNode {
+        final public Frame frame;
+        final public StackNode prior;
+
+        public StackNode(StackNode prior) {
+            this.frame = new Frame();
+            this.prior = prior;
+        }
     }
+    
+    private StackNode topFrame = null;
 
     public void pushFrame() {
-        this.topFrame = new Frame(this.topFrame);
-        this.topFrame.copyFromPrior();
+        final StackNode node = new StackNode(this.topFrame);
+        if (this.topFrame != null)
+            node.frame.copyFrom(this.topFrame.frame);
+        this.topFrame = node;
+    }
+    
+    public void pushFrameCopy(Frame copy) throws Exception {
+        final StackNode node = new StackNode(this.topFrame);
+        if (copy != null)
+            node.frame.copyFrom(copy);
+        this.topFrame = node;
     }
 
     public void pushCleanFrame() {
-        this.topFrame = new Frame(this.topFrame);
+        this.topFrame = new StackNode(this.topFrame);
     }
 
     public void pushImmediateFrame() {
-        this.topFrame = new Frame(this.topFrame);
-        this.topFrame.copyImmediateFromPrior();
+        final StackNode node = new StackNode(this.topFrame);
+        if (this.topFrame != null)
+            node.frame.copyImmediateFrom(this.topFrame.frame);
+        this.topFrame = node;
     }
 
     public void popFrame() throws Exception {
         Require.notNull(this.topFrame, "instantiator has no frame to pop");
         this.topFrame = this.topFrame.prior;
+    }
+
+    public Frame copyFrame() {
+        Frame frame = new Frame();
+        if (this.topFrame != null)
+            frame.copyFrom(this.topFrame.frame);
+        return frame;
     }
 
     /**
@@ -137,11 +155,13 @@ public class Instantiator {
         Require.notNull(this.topFrame, "cannot add to an empty instantiator");
         Require.notNull(param, "can not have a null type parameter in an instantiator frame");
         Require.notNull(arg, "can not have a null the argument in an instantiator frame");
-        this.topFrame.add(param, arg);
+        if (this.topFrame.prior != null)
+            arg = this.topFrame.prior.frame.replace(arg);
+        this.topFrame.frame.add(param, arg);
     }
 
     public Ref<? extends TypeDesc> replace(Ref<? extends TypeDesc> con) {
-        return this.topFrame == null ? con : this.topFrame.replace(con);
+        return this.topFrame == null ? con : this.topFrame.frame.replace(con);
     }
 
     public List<Ref<? extends TypeDesc>> typeArgs() throws Exception {
@@ -150,13 +170,13 @@ public class Instantiator {
 
     public List<Ref<? extends TypeDesc>> typeArgs(boolean withNest) throws Exception {
         if (this.topFrame == null) return Collections.emptyList();
-        if (withNest) return this.topFrame.typeArgsWithNest();
-        return this.topFrame.immediateTypeArgs();
+        if (withNest) return this.topFrame.frame.typeArgsWithNest();
+        return this.topFrame.frame.immediateTypeArgs();
     }
 
     @Override
     public String toString() {
         if (this.topFrame == null) return "<null>";
-        return this.topFrame.toString();
+        return this.topFrame.frame.toString();
     }
 }
